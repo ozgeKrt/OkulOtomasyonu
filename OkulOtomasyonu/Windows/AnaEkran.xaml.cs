704b370ea519362ffd7cc4992aa4bbcd5a47ed48
ozge <ozgekurt@skoptek.com>
2020-09-30 18:18:26 +0300

    ilk

---
+++ OkulOtomasyonu/Windows/AnaEkran.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OkulOtomasyonu.Windows
+{
+    /// <summary>
+    /// Interaction logic for AnaEkran.xaml
+    /// </summary>
+    public partial class AnaEkran : Window
+    {
+        public AnaEkran()
+        {
+            InitializeComponent();
+            menuKapaliMi = true;
+        }
+
+        public bool menuKapaliMi { get; set; }
+
+        private void Rectangle_PreviewDragOver(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void Rectangle_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove(); //ekranı tutup hareket ettşrmek için buyuk dikdortgen
+        }
+
+        private void menuButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void menuButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (!menuKapaliMi)
+            {
+                Storyboard storyboard = this.FindResource("menuAc") as Storyboard;
+                storyboard.Begin();
+            }
+            else if (menuKapaliMi)
+            {
+                Storyboard storyboard = this.FindResource("menuKapat") as Storyboard;
+                storyboard.Begin();
+
+            }
+            menuKapaliMi = !menuKapaliMi; //menukapalımı: false ise true cevirir
+        }
+    }
+}
---
+++ OkulOtomasyonu/Windows/Giris.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OkulOtomasyonu.Windows
+{
+    /// <summary>
+    /// Interaction logic for Giris.xaml
+    /// </summary>
+    public partial class Giris : Window
+    {
+        public Giris()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBlock_GiveFeedback(object sender, GiveFeedbackEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);    //cikis butonu
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            AnaEkran ana = new AnaEkran();
+            ana.Show();
+            this.Close();
+        }
+    }
+}
